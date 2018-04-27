d7a0ad8c3a139a72f3a259025f44111c6cc37616
szc0098 <szc0098@tigermail.auburn.edu>
2018-04-27 11:52:58 -0500

    Created imageSubmission class for retrieving the grid  view

---
+++ src/Models/ImageSubmission.java
@@ -0,0 +1,29 @@
+package Models;
+
+public class ImageSubmission {
+	String image;
+	String description;
+	int subID;
+	
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getSubID() {
+		return subID;
+	}
+	public void setSubID(int subID) {
+		this.subID = subID;
+	}
+	
+	
+
+}
